fe1faf77e19630fb74c55a797a55a044473e4f7f
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2022-05-19 18:06:20 +0200

    feat: create comment response changed

--- ApiGateway/src/main/java/com/apigateway/dto/CommentResponseDTO.java
+++ ApiGateway/src/main/java/com/apigateway/dto/CommentResponseDTO.java
@@ -1,8 +1,14 @@
 package com.apigateway.dto;
 
+import proto.UserNamesResponseProto;
+
 public class CommentResponseDTO {
 
     private String text;
+
+    private String firstName;
+
+    private String lastName;
 
     public CommentResponseDTO() {
     }
@@ -11,11 +17,33 @@ public class CommentResponseDTO {
         this.text = text;
     }
 
+    public CommentResponseDTO(String comment, UserNamesResponseProto userNames) {
+        this.text = comment;
+        this.firstName = userNames.getFirstName();
+        this.lastName = userNames.getLastName();
+    }
+
     public String getText() {
         return text;
     }
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
