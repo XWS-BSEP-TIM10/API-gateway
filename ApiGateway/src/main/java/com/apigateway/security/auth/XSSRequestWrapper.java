c98116b663490646a9c96c35c2c828e306fca4bd
Kina999 <katarinazer6@gmail.com>
2022-05-20 20:04:15 +0200

    fix: xss fixed

--- ApiGateway/src/main/java/com/apigateway/security/auth/XSSRequestWrapper.java
+++ ApiGateway/src/main/java/com/apigateway/security/auth/XSSRequestWrapper.java
@@ -13,115 +13,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class XSSRequestWrapper extends HttpServletRequestWrapper {
-private Logger logger = LoggerFactory.getLogger(XSSRequestWrapper.class);
-	
-	public XSSRequestWrapper(HttpServletRequest request) {
-		super(request);
+	public XSSRequestWrapper(HttpServletRequest servletRequest) {
+		super(servletRequest);
 	}
 
-	private static Pattern[] patterns = new Pattern[]{
-			// Script, Style fragments
-	        Pattern.compile("(<style>|<script>)((.|[\\r\\n])*?)(</style>|</script>)", Pattern.CASE_INSENSITIVE),
-	        // src='...', href='...', style='...'
-	        Pattern.compile("(style|href|src)((\\s|[\\r\\n])*?)=(\\s|[\\r\\n])*(\\'|\\\")((.|[\\r\\n])*?)(\\'|\\\")", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // DOM
-	        Pattern.compile("(document|attr|element|nodemap|nodelist|text|entity|namemap).(.*?)[\\s|=|)]", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // lonely script and style tags
-	        Pattern.compile("</script>|</style>", Pattern.CASE_INSENSITIVE),
-	        Pattern.compile("<script(.*?)|<style(.*?)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // eval(...), expression(...)
-	        Pattern.compile("(eval|expression)\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // javascript:... , vbscript:...
-	        Pattern.compile("(javascript|vbscript):", Pattern.CASE_INSENSITIVE),
-	        // on...(...)=...
-	        Pattern.compile("on((.|[\\r\\n])*?)=((.|[\\r\\n])*?)[\\'|\\\"]((.|[\\r\\n])*?)[\\'|\\\"]", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // body, img, iframe, input, link, table, td, div, a tag
-	        Pattern.compile("<(meta|html|body|iframe|img|input|link|object|a|div|td|th|span)((.|[\\r\\n])*)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // html tags
-	        Pattern.compile("<((.|[\\r\\n])*?)>((.|[\\r\\n])*?)</((.|[\\r\\n])*)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // CDATA
-	        Pattern.compile("<!\\[CDATA((.|[\\r\\n])*?)\\]>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        // everything between < & > if escaped from above patterns
-	        Pattern.compile("<((.|[\\r\\n])*)(>|\\\")", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        //Pattern.compile("\\[((.|[\\r\\n])*)(\\]|\\\")", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-	        //Pattern.compile("<|>|\\[|\\]|&|\"|'|%|\\(|\\)|\\+", Pattern.MULTILINE | Pattern.DOTALL)
-	    };
+	@Override
+	public String[] getParameterValues(String parameter) {
+		String[] values = super.getParameterValues(parameter);
 
-	    @Override
-	    public String[] getParameterValues(String parameter) {
-	    	try {
-		        String[] values = super.getParameterValues(parameter);
-	
-		        if (values == null) {
-		            return null;
-		        }
-	
-		        int count = values.length;
-		        String[] encodedValues = new String[count];
-		        for (int i = 0; i < count; i++) {
-		            encodedValues[i] = stripXSS(values[i]);
-		        }
-		        return encodedValues;
-	    	} catch (Exception e) {
-	    		logger.debug(this.getClass().getName(), "getParameterValues", "Entering into TxnHistory");
-	    	}
-	        return null;
-	    }
+		if (values == null) {
+			return null;
+		}
 
-	    @Override
-	    public String getParameter(String parameter) {
-	        String value = super.getParameter(parameter);
-	        return stripXSS(value);
-	    }
+		int count = values.length;
+		String[] encodedValues = new String[count];
+		for (int i = 0; i < count; i++) {
+			encodedValues[i] = replaceXSSCharacters((values[i]));
+		}
 
-	    @Override
-	    public String getHeader(String name) {
-	        String value = super.getHeader(name);
-	        return stripXSS(value);
-	    }
-	    
-	 
+		return encodedValues;
+	}
 
-	    public String stripXSS(String value) {
-	        if (value != null) {
-	        	
-	            // Avoid null characters
-	            value = value.replaceAll("", "");
+	private String replaceXSSCharacters(String value) {
+		if (value == null) {
+			return null;
+		}
+		return value
+				.replace("&", "&#38;")
+				.replace("<", "&#60;")
+				.replace(">", "&#62;")
+				.replace("\"", "&#34;")
+				.replace("'", "&#39;");
+	}
 
-	            // Remove all sections that match a pattern
-	            for (Pattern scriptPattern : patterns){
-	                value = scriptPattern.matcher(value).replaceAll("");
-	            }
-	            //value = Encode.forHtml(value);
-	        }
-	        return value;
-	    }
-	
-	  public boolean isMatch() {
-		  Map<String, String[]> params = super.getParameterMap();
-		  for(String key: params.keySet()) {
-			  for(String value: params.get(key)) {
-				  if(checkPattern(value)) return true;
-			  }
-		  }
-		  return false;
-	  }
+	@Override
+	public String getParameter(String parameter) {
+		return replaceXSSCharacters(super.getParameter(parameter));
+	}
 
-	  private boolean checkPattern(String value) {
-		  if (value != null) {
-	            // Avoid null characters
-	            value = value.replaceAll("", "");
-
-	            // Remove all sections that match a pattern
-	            Matcher matcher = null;
-	            for (Pattern scriptPattern : patterns){
-	            	matcher = scriptPattern.matcher(value);
-	            	if(matcher.find()) {
-	            		return true;
-	            	}
-	            }
-	        }
-	        return false;
-	  }
-
-	 }
+	@Override
+	public String getHeader(String name) {
+		return replaceXSSCharacters(super.getHeader(name));
+	}
+}
--- ApiGateway/src/main/java/com/apigateway/security/config/XSSFilter.java
+++ ApiGateway/src/main/java/com/apigateway/security/config/XSSFilter.java
@@ -18,54 +18,16 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class XSSFilter implements Filter {
 
-	/**
-	 * Default constructor.
-	 */
-	public XSSFilter() {
-		// TODO Auto-generated constructor stub
-	}
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException { }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
+	@Override
+	public void destroy() { }
+
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-		// Deny ClickJacking Attacks from URL's outside our application domain
-		HttpServletResponse res = (HttpServletResponse) response;
-		//String context2=request.get
-		XSSRequestWrapper reqWrapper = new XSSRequestWrapper((HttpServletRequest) request);
-		
-
-		if(!reqWrapper.getRequestURI().contains("error")){
-			if (reqWrapper.isMatch()) {
-				res.reset();
-				res.addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
-				res.addHeader("X-XSS-Protection", "1; mode=block");
-				res.addHeader("X-Content-Type-Options", "nosniff");
-				res.setStatus(HttpServletResponse.SC_BAD_REQUEST);	
-				res.sendError(HttpServletResponse.SC_BAD_REQUEST);
-				//request.getRequestDispatcher("error").forward(request, res);
-				//res.sendRedirect(request.getServletContext().getContextPath() + "/jsp/exception/403.jsp");
-				return;
-			}
-		}
-		res.addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
-		res.addHeader("X-XSS-Protection", "1; mode=block");
-		res.addHeader("X-Content-Type-Options", "nosniff");
-		chain.doFilter(request, res);
+		chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
 }
